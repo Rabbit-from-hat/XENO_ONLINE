9395e5d244e51df14a8bd9ec909c2f4ccde5f139
N.Okamoto <magicianchild1412@gmail.com>
2020-04-20 11:15:32 +0900

    first push

---
+++ js/main.js
@@ -0,0 +1,39 @@
+'use strict';
+
+{
+  class CardGame {
+
+    constructor() {
+      this.deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 1, 2, 3, 4, 5, 6, 7, 8];
+      this.player1Hand = null;
+      this.player2Hand = null;
+    }
+
+    show() {
+      document.getElementById('deck_num').textContent = this.deck.length + "枚"
+      document.getElementById('player1_hand').textContent = "[" + this.player1Hand + "]"
+      document.getElementById('player2_hand').textContent = "[" + this.player2Hand + "]"
+    }
+
+    shuffle() {
+      for (let i = this.deck.length - 1; i >= 0; i--){
+        const j = Math.floor(Math.random() * (i + 1));
+        [this.deck[i], this.deck[j]] = [this.deck[j], this.deck[i]];
+      }
+    }
+
+    deal() {
+      this.player1Hand = this.deck.shift();
+      this.player2Hand = this.deck.shift();
+    }
+  }
+
+  const game = new CardGame();
+
+  document.getElementById('start_button').addEventListener('click', () => {
+    game.shuffle();
+    game.deal();
+    game.show();
+    document.getElementById('start_button').style.display = "none";
+  });
+}
